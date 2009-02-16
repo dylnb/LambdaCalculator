f8019ab4c661c438e48c589b6e92e86c3844f8a2
champoll <champoll@60b5715d-1827-0410-936c-a9fbbdc1ca8b>
2009-02-16 23:05:06 +0000

    Fixed bug in toString representation of explicitly typed variables

--- src/lambdacalc/logic/Identifier.java
+++ src/lambdacalc/logic/Identifier.java
@@ -93,7 +93,7 @@ public abstract class Identifier extends Expr {
         } else if (mode == LATEX) {
             return this.symbol + "_{" + type.toLatexString() + "}";
         } else { // mode == TXT
-            return this.symbol + "_" + type.toString() + "}";
+            return this.symbol + "_" + type.toShortString();
         }
     }
     
