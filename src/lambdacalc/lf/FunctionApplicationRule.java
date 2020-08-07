637766f84f8f1157bef449b6c0325f51253559c0
Anna Alsop <aua214@nyu.edu>
2020-08-07 15:10:32 -0400

    update function application rule

--- src/lambdacalc/lf/FunctionApplicationRule.java
+++ src/lambdacalc/lf/FunctionApplicationRule.java
@@ -153,12 +153,15 @@ public class FunctionApplicationRule extends CompositionRule {
             Type r = right.getType();
             if (l instanceof CompositeType) {
                 CompositeType t = (CompositeType)l;
-                if (t.getLeft().equals(r)) {
-                    // Call to alignTypes ensures that an error is thrown if the same VarType
-                    // is matched to multiple constant types
-                    HashMap<Type,Type> typeMatches = Expr.alignTypes(t.getLeft(),r);
+                if (t.getLeft().matches(r) != null) {
                     return true;
                 }
+//                if (t.getLeft().equals(r)) {
+//                    // Call to alignTypes ensures that an error is thrown if the same VarType
+//                    // is matched to multiple constant types
+//                    HashMap<Type,Type> typeMatches = Expr.alignTypes(t.getLeft(),r);
+//                    return true;
+//                }
                 else if (t.getLeft() instanceof ProductType){
                     ProductType pt = (ProductType)t.getLeft();
                     if (Arrays.asList(pt.getSubTypes()).contains(r)) {
@@ -167,8 +170,7 @@ public class FunctionApplicationRule extends CompositionRule {
                 }
             }
         } catch (TypeEvaluationException ex) {
-        } catch (MeaningEvaluationException me) {
-        }
+        } 
         return false;
     }
     
